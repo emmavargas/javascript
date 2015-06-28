9c419f54182f8b2219876abd01970cb9eedb8803
Emma Vargas <emma.vargasvasquez@gmail.com>
2015-06-27 21:53:41 -0300

    Commit quinto

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 var letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T"];
-var dniUsuario = 38285719;
-var letraUsuario = "R";
-//var letra =
-if(dni <= 0 || dni > 99999999) {
+var dniUsuario = prompt("Introduce tu numero de DNI (sin la letra)");
+var letraUsuario = prompt("Introduce tu letra de tu DNI (en mayuscula)");
+letra = letra.toUpperCase();
+
+if(dniUsuario <= 0 || dniUsuario > 99999999) {
   alert("numero no valido");
 }
 else {
