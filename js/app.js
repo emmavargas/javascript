71f071541a80f5058d3641add2cca017a6894729
Emma Vargas <emma.vargasvasquez@gmail.com>
2015-06-27 21:41:53 -0300

    Commit quinto

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 var letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T"];
 var dniUsuario = 38285719;
 var letraUsuario = "R";
-var resultado = dniUsuario % 23;
-var letra =
+//var letra =
 if(dni <= 0 || dni > 99999999) {
   alert("numero no valido");
+}
+else {
+    var numeroCalculado = dniUsuario % 23;
+    var resultado = letras[numeroCalculado];
+    if (resultado == letraUsuario) {
+      alert("Hubo Coincidencia");
+    }
+    else {
+      alert("No hubo coincidencia");
+    }
 }
