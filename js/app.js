47b3eaa9f741759109e69d1aa0353c522e5755e1
Emma Vargas <emma.vargasvasquez@gmail.com>
2015-06-27 21:24:51 -0300

    Commit cuarto

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 var letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T"];
+var dniUsuario = 38285719;
+var letraUsuario = "R";
+var resultado = dniUsuario % 23;
+var letra =
+if(dni <= 0 || dni > 99999999) {
+  alert("numero no valido");
+}
